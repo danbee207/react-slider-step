e3e9921562223339ebac2d92c25354b233fd8f57
danbee207 <danpark@dh017-161.medilab.tlt.stonybrook.edu>
2016-04-15 15:30:24 -0400

    INIT
    +WEB PACK

---
+++ src/components/ReactSliderStepComponent.js
@@ -0,0 +1,19 @@
+'use strict';
+
+import React from 'react';
+
+require('styles//ReactSliderStep.sass');
+
+let ReactSliderStepComponent = (props) => (
+  <div className="reactsliderstep-component">
+    Please edit src/components///ReactSliderStepComponent.js to update this component!
+  </div>
+);
+
+ReactSliderStepComponent.displayName = 'ReactSliderStepComponent';
+
+// Uncomment properties you need
+// ReactSliderStepComponent.propTypes = {};
+// ReactSliderStepComponent.defaultProps = {};
+
+export default ReactSliderStepComponent;
